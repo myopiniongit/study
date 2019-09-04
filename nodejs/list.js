8001fbab1242641c07d960ee48429a9a41cf966d
PeterKimG <myopinion@naver.com>
2019-09-04 15:48:52 +0900

    commit

--- nodejs/list.js
+++
@@ -1,8 +0,0 @@
-list: function (filelist) {
-    var list = '<ul>';
-    for (i = 0; i < filelist.length; i++) {
-      list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`
-    }
-    list = list + '</ul>'
-    return list
-  }
